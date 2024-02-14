3cb2ce89504f801f0c61ad92b617d24d84cd48de
shafeequem <153593452+shafeequem@users.noreply.github.com>
2024-02-14 16:14:09 +0400

    tofu

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
 resource "aws_vpc" "main" {
-  # Referencing the base_cidr_block variable allows the network address
-  # to be changed without modifying the configuration.
   cidr_block = var.base_cidr_block
    tags = {
     Name = "tofu-vpc"
@@ -8,19 +6,8 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "az" {
-  # Create one subnet for each given availability zone.
   count = length(var.availability_zones)
-
-  # For each subnet, use one of the specified availability zones.
   availability_zone = var.availability_zones[count.index]
-
-  # By referencing the aws_vpc.main object, OpenTofu knows that the subnet
-  # must be created only after the VPC is created.
   vpc_id = aws_vpc.main.id
-
-  # Built-in functions and operators can be used for simple transformations of
-  # values, such as computing a subnet address. Here we create a /20 prefix for
-  # each subnet, using consecutive addresses for each availability zone,
-  # such as 10.1.16.0/20 .
   cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 4, count.index+1)
 }
