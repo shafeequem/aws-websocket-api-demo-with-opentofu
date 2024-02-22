b5dcff0dac38971cd1cf204f6b21c82e2c5e9475
shafeequem <153593452+shafeequem@users.noreply.github.com>
2024-02-22 22:20:21 +0400

    minor tf changes

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,8 @@
+/*
 output "ws_api_url" {
   value = aws_apigatewayv2_stage.ws_messenger_api_stage.invoke_url
 }
+*/
 
 output "website_url" {
   value = "http://${aws_s3_bucket.ws_app.bucket}.s3-website.${var.aws_region}.amazonaws.com"
