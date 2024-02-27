7fef53d4b844f70e04a068ba33db08872e5b0663
shafeequem <153593452+shafeequem@users.noreply.github.com>
2024-02-27 16:21:07 +0400

    tofu output modified

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,7 @@ output "ws_api_url" {
 output "website_url" {
   value = "http://${aws_s3_bucket.ws_app.bucket}.s3-website.${var.aws_region}.amazonaws.com"
 }
+
+output "messenger_lambda" {
+  value = aws_lambda_function.ws_messenger_lambda.function_name
+}
